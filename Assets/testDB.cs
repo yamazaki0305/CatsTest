fd4b14a280e0502d4087ab8b6dcc39e9f97f3f77
yamazaki0305 <yamazaki0305@hotmail.com>
2018-09-22 17:18:41 +0900

    SQLiteの試す　 DllNotFoundException: sqlite3 SqliteDatabase.Open (System.String path) (at Assets/Scripts/SQLites/SqliteDatabase.cs:146) SqliteDatabase.Open () (at Assets/Scripts/SQLites/SqliteDatabase.cs:137) SqliteDatabase.ExecuteQuery (System.String query) (at Assets/Scripts/SQLites/SqliteDatabase.cs:212) testDB.Start () (at Assets/testDB.cs:19)

---
+++ Assets/testDB.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class testDB : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        SqliteDatabase sqlDB = new SqliteDatabase("GameMaster.db");
+        string query = "select name,attack from Weapon where id=2";
+        DataTable dataTable = sqlDB.ExecuteQuery(query);
+
+        string name = "";
+        int attack = 0;
+        foreach (DataRow dr in dataTable.Rows)
+        {
+            name = (string)dr["name"];
+            attack = (int)dr["attack"];
+            Debug.Log("name:" + name + " attack:" + attack);
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
