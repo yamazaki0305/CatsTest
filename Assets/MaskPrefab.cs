1a88bdc084c14af5771cdf38b83c0ae10acf65f0
yamazaki0305 <yamazaki0305@hotmail.com>
2018-10-06 20:03:55 +0900

    とりあえず現状までコミット

--- Assets/MaskPrefab.cs
+++ Assets/MaskPrefab.cs
@@ -16,7 +16,7 @@ public class MaskPrefab : MonoBehaviour {
 	void Start () {
 
         var color = this.GetComponent<SpriteRenderer>().color;
-        //color.a = 0.5f;
+        color.a = 0.3f;
         this.GetComponent<SpriteRenderer>().color = color;
     }
 	
